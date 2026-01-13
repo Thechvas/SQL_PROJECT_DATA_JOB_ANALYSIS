beb98da6c0b16385b57163ec95bfc427ee8e02b6
kropfkevin11@stud.uni-obuda.hu <kropfkevin11@stud.uni-obuda.hu>
2026-01-13 17:35:40 +0100

    first commit

---
+++ sql_load/3_modify_tables.sql
@@ -0,0 +1,28 @@
+FROM 'C:\Users\kropf\Desktop\Training\csv_files\company_dim.csv' WITH (
+        FORMAT csv,
+        HEADER true,
+        DELIMITER ',',
+        ENCODING 'UTF8'
+    );
+COPY skills_dim
+FROM 'C:\Users\kropf\Desktop\Training\csv_files\skills_dim.csv' WITH (
+        FORMAT csv,
+        HEADER true,
+        DELIMITER ',',
+        ENCODING 'UTF8'
+    );
+COPY job_postings_fact
+FROM 'C:\Users\kropf\Desktop\Training\csv_files\job_postings_fact.csv' WITH (
+        FORMAT csv,
+        HEADER true,
+        DELIMITER ',',
+        ENCODING 'UTF8'
+    );
+COPY skills_job_dim
+FROM 'C:\Users\kropf\Desktop\Training\csv_files\skills_job_dim.csv' WITH (
+        FORMAT csv,
+        HEADER true,
+        DELIMITER ',',
+        ENCODING 'UTF8'
+    );
+-- SELECT * FROM job_postings_fact LIMIT 100;
